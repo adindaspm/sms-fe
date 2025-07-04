f86c0763f1fce12a284a7366eedeca03bc3577e9
adindaspm <222111844@stis.ac.id>
2025-07-04 11:22:03 +0700

    check URL

--- services/authService.js
+++ services/authService.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const { apiBaseUrl } = require('../config');
 
 async function loginAndSave(email, password) {
+  console.log(`${apiBaseUrl}/login`)
   const response = await axios.post(`${apiBaseUrl}/login`, {
     email,
     password
